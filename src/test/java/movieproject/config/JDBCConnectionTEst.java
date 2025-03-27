e4f9f46aef38fc738206aea28b2f23a288a2b6e0
jaanja <sungjun345@gmail.com>
2025-03-27 13:14:53 +0900

    DB Connection 설정

---
+++ src/test/java/movieproject/config/JDBCConnectionTEst.java
@@ -0,0 +1,36 @@
+package movieproject.config;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.movieproject.config.JDBCConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JDBCConnectionTEst {
+
+    @BeforeAll
+    static void setUp() {
+        System.out.println("테스트 시작");
+    }
+
+
+    @Test
+    void testConnection() throws SQLException {
+        try(Connection con = JDBCConnection.getConnection()){
+            Assertions.assertNotNull(con, "connection이 생성되지 않았습니다.");
+            Assertions.assertFalse(con.isClosed(), "커넥션은 열려있어야 합니다.");
+            System.out.println("DB 커넥션이 연결되었습니다.");
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("테스트 종료 및 자원 반납");
+        JDBCConnection.close();
+    }
+}
